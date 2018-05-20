b231706bc407ff19212f8f486bde23a2bb823220
Olegzandr VD <alex@oresoftware.com>
2018-05-20 14:51:55 -0700

    0.0.106

--- lib/helper.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { OplogInterpreter, OplogInterpreterOpts } from "./interfaces";
-import { Stream } from "stream";
-import { Timestamp } from "bson";
-import { Collection } from "mongodb";
-export declare const getValidTimestamp: (ts: any, coll: Collection<any>) => Timestamp;
-export declare const getOplogStreamInterpreter: (s: Stream, opts?: OplogInterpreterOpts) => OplogInterpreter;
